985015d8ee6ed32504cf666c65cb82d552a84fb8
Muhammad Bilal <muhammadb159@gmail.com>
2020-11-12 12:14:05 +0500

    first commit

---
+++ app/src/main/java/com/example/myproxywithactivity/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myproxywithactivity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+private class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            startActivity(Intent(this, MainActivity2::class.java))
+        }
+
+
+    }
+
+}
